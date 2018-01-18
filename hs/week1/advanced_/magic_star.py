707f35b450e56d3961c8fd893500ba38b0bfe299
Hyun Soo Chang <zenith11311@gmail.com>
2018-01-18 17:38:09 +0900

    ´ٸ¥ ȯ°濡¼­ °³¹ßÇϱâ À§ÇØ ÀϴÜ Ŀ¹Ô

---
+++ hs/week1/advanced_/magic_star.py
@@ -0,0 +1,38 @@
+'''
+    @ filename : magic_star.py
+    @ brief    : try to make star with sides that has same value for sum(included_nodes);
+    @ author   : Caelum
+    @ date     : 180117
+
+sample input
+....x....
+.A.I.D.x.
+..x...x..
+.x.x.x.x.
+....x....
+
+expected ouput
+....F....
+.A.I.D.L.
+..H...E..
+.C.J.B.K.
+....G....
+'''
+
+matrix = ""
+a = 
+
+def sum(idx0, idx1, idx2, idx3) :
+    return ord(matrix[idx0])+ord(matrix[idx1])+ord(matrix[idx2])+ord(matrix[idx3])
+
+def validation(
+
+for i in range(0, 4):
+    matrix += raw_input()
+
+
+print matrix
+print sum(0, 2, 3, 4)
+
+
+
