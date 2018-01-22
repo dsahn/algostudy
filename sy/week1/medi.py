7f4044e392c8496a114b7dc41c2c49d5c4135915
jsy9kr2004 <35419168+jsy9kr2004@users.noreply.github.com>
2018-01-22 17:45:20 +0900

    문제의 input 형식에 맞게 변경

--- sy/week1/medi.py
+++ sy/week1/medi.py
@@ -1,4 +1,3 @@
-
 def pick (w, h) :
     ways = 0
     if w < 0 or h < 0:
@@ -7,8 +6,13 @@ def pick (w, h) :
         return 1
     else :
        return pick(w-1, h+1) + pick(w, h-1)
-#ways += pick(w, h-1, 2)
-#return ways
 
-n = int(input("Enter a number : "))
-print(pick(n,0))
+input_list = []
+while 1:
+    input_num = int(input())
+    if input_num == 0:
+        break
+    input_list.append(input_num)
+
+for input_num in input_list:
+    print(pick(input_num, 0))
