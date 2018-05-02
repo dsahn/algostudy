d0baefd6c7bfa111474643cdaf54ffc67d779ff6
DONGSU AN <ads50112@gmail.com>
2018-05-03 00:40:07 +0900

    6번 문제 풀어서 제출
    재귀로 풀어보았다. 레퍼런스 형태로 인자를 받아 복잡하지만
    로직 자체는 복잡하지 않은듯.

--- ds/cracking-the-coding-interview/2-linked-list/6.cpp
+++ ds/cracking-the-coding-interview/2-linked-list/6.cpp
@@ -8,6 +8,14 @@
  *      배열을 array 처럼 표현하여 접근하는것은 문제의 의도가 아닐거같은데..
  *      리스트의 전체 길이는 알 수 있으니까, 중간에 접근하는건 가능할 듯.
  *      중간부터 스택과 next를 이용하여 표현해보자
+ * fail1: 
+ * - _is_palindrome 내부에 int_list.size() 호출..
+ *      size를 얻을 다른 인자를 넣어주자
+ * - ->,. operator 혼동
+ * fail2:
+ * - segfault 발생.
+ *   - 하나 있는 경우를 테스트 케이스에 넣어서 그런듯.
+ *
  * */
 
 #include <iostream>
@@ -15,22 +23,53 @@
 #include <algorithm>
 #include "linked_list.h"
 
+bool _is_palindrome(SinglePtrNode<int>* prev, int pos, int size, 
+        SinglePtrNode<int>* next) 
+{
+    if (pos >= size / 2) {
+        if (next != nullptr)
+            *next = *prev->next;
+        return true;
+    }
+    else {
+        SinglePtrNode<int> outer(0);
+        bool before_val = _is_palindrome(prev->next, pos+1, size, &outer);
+        if (pos != 0)
+            *next = *outer.next;
+        return before_val && (outer.elem == prev->elem); 
+    }
+}
+
+bool is_palindrome(SingleLinkedList<int> int_list) {
+    return _is_palindrome(int_list.head(), 0, int_list.size(), nullptr);
+}
+
 using namespace std;
 
 int main() {
 	SingleLinkedList<int> must_true;
 	SingleLinkedList<int> must_false;
+	SingleLinkedList<int> single;
 
     must_true.push_back(1);
     must_true.push_back(2);
     must_true.push_back(3); 
-    must_true.push_back(4); 
-    must_true.push_back(5); 
+    must_true.push_back(2); 
+    must_true.push_back(1); 
 
     must_false.push_back(3); 
+    must_false.push_back(2); 
+    must_false.push_back(2); 
 
-    is_palindrome(must_true);
-    is_palindrome(must_false);
+    single.push_back(2); 
+
+    bool val;
+    val = is_palindrome(must_true);
+    cout << val << endl;
+    val = is_palindrome(must_false);
+    cout << val << endl;
+    val = is_palindrome(single);
+    cout << val << endl;
 
 	return 0;
 }
