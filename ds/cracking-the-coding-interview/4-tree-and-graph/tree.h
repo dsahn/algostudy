fcdafb27a05909922ccf2732390f199980787b78
DONGSU AN <ads50112@gmail.com>
2018-05-27 19:00:43 +0900

    print_in_tree(), head() 추가

--- ds/cracking-the-coding-interview/4-tree-and-graph/tree.h
+++ ds/cracking-the-coding-interview/4-tree-and-graph/tree.h
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <iomanip>
 
 template <typename T>
 struct BinTreeNode {
@@ -31,9 +32,47 @@ struct BinTreeNode {
 template <typename T>
 class BinTree {
 public:
+    BinTree() : _head(nullptr) {}
     // 노드로 이미 트리를 만든 형태로만 트리를 생성하자
     BinTree(BinTreeNode<T>* head) :_head(head) {} 
-    void print() { std::cout << _head->print() << std::endl; }
+
+    BinTreeNode<T>* head() { return _head; }
+    void print() { _head->print(); }
+    void insert_with_root(T data, BinTreeNode<T>*& root) {
+        if (root == nullptr)
+            root = new BinTreeNode<T>(data);
+        else {
+            if (data < root->elem)
+                insert_with_root(data, root->left);
+            else
+                insert_with_root(data, root->right);
+        }
+    }
+    void insert(T data) {
+        insert_with_root(data, _head);
+    }
+
+    void postorder(BinTreeNode<T>* p, int indent=0)
+    {
+        if(p != NULL) {
+            if(p->right) {
+                postorder(p->right, indent+4);
+            }
+            if (indent) {
+                std::cout << std::setw(indent) << ' ';
+            }
+            if (p->right) std::cout<<" /\n" << std::setw(indent) << ' ';
+            std::cout<< p->elem << "\n ";
+            if(p->left) {
+                std::cout << std::setw(indent) << ' ' <<" \\\n";
+                postorder(p->left, indent+4);
+            }
+        }
+    }
+
+    void print_in_tree() {
+        postorder(_head);
+    }
 private:
     BinTreeNode<T> *_head;
 };
